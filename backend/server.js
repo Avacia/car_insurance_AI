a6ee558ff0ac6ffcc9e04e608e31b2e436af5bad
Avacia <yenw@missionreadyhq.com>
2024-08-13 20:14:49 +1200

    fixed docker problem

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 
 /* ======================================== Port ======================================== */
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
