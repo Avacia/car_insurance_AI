a2a05601b8df21f9890c4427d87359a6cf524abf
Matias Pereyra <matiasv@missionreadyhq.com>
2024-08-20 18:02:50 +1200

    add fech to backend and chat display

--- backend/Controller/Chat.js
+++ backend/Controller/Chat.js
@@ -1,14 +1,17 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+require('dotenv').config()
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY );
 
-export default async function handler(req, res) {
+module.exports.handler = async(req, res) => {
   
     const { message } = req.body;
+    console.log(message)
 
     try {
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       const result = await model.generateContent(message);
+      
       const response = await result.response;
       const text = response.text();
 
@@ -18,4 +21,4 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'An error occurred while processing your request.' });
     }
 
-}
+};
--- frontend/src/component/AI/ai.jsx
+++ frontend/src/component/AI/ai.jsx
@@ -3,14 +3,74 @@ import Lottie from 'lottie-react'
 import ContactUs from '../../assets/animation/Animation - contactUs.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRobot, faMessage, faXmark, faUpLong } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function AI(){
     const [isOpen, setIsOpen] = useState(false);
+    const [userInput, setUserInput] = useState('');
+    const [chatPrompts, setChatPrompts] = useState([{ role: 'system', content: "You are Tinnie, an AI that helps users choose the best insurance policy." }]);
+    const [chatBool, setChatBool] = useState(false);
+
+    
+
 
     function handleClick(){
         setIsOpen(!isOpen);
+        if(chatPrompts.length === 1){
+            sendMessageToAI(chatPrompts[0].content)
+        }
+    }
+
+
+    function userInputContent(e){
+        setUserInput(e.target.value)
+    }
+
+    const handleSubmit = (e) => { 
+        e.preventDefault();
+        if(userInput != ''){
+            setChatPrompts([...chatPrompts, { role: 'user', content: userInput}]);
+            setChatBool(true);
+            
+        }
+
+    }    
+
+    useEffect(() => {
+        console.log(chatPrompts)
+        if(chatPrompts.length > 0 && chatBool && userInput != ''){
+            sendMessageToAI(userInput)
+            setChatBool(false);
+            setUserInput('');
+        }
+        
+    }, [chatPrompts])
+
+
+    async function sendMessageToAI(userInput){
+    
+        const options = {
+        method: 'POST',
+        body: JSON.stringify({
+            message: userInput
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            }
+        }
+    
+        try{
+            const response = await fetch('http://localhost:4000/chat', options)
+            const data = await response.json()
+          //console.log(data)
+            setChatPrompts([...chatPrompts, { role: 'assistant', content: data.message}]);
+            console.log(data.message)
+            
+        }
+        catch(error){
+            console.log(error)
+        }
     }
 
     return(
@@ -41,12 +101,22 @@ export default function AI(){
                     </div>
 
                     <div className={style.chatbotContentMessage}>
-                        <p>space for content</p>
+                    <ul className="chat">
+                        {chatPrompts.length > 0 && chatPrompts.map((message, index) => {
+                            console.log(message.content)
+                            return(
+                                
+                                <li key={index}>
+                                    {message.content}
+                                </li>
+                            )
+                        })}
+                        </ul>  
                     </div>
 
                     <div className={style.chatbotContentUserInput}>
-                        <input className={style.inputField} type="text" placeholder="Type your message here..." />
-                        <button className={style.sendBtn}><FontAwesomeIcon icon={faUpLong} size='x' /></button>
+                        <input className={style.inputField} type="text" value={userInput} onChange={userInputContent} placeholder="Type your message here..." />
+                        <button className={style.sendBtn} onClick={handleSubmit}>><FontAwesomeIcon icon={faUpLong} size='x' /></button>
                     </div>
                 </div>}
             </div>
