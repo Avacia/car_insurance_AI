449b5fcf06191c21c70127e749fc8250c04cb2e7
Avacia <yenw@missionreadyhq.com>
2024-08-22 01:16:09 +1200

    finalize frontend design

--- frontend/src/component/Card/card.jsx
+++ frontend/src/component/Card/card.jsx
@@ -7,16 +7,19 @@ export default function card() {
             name: "MBI (Mechanical Breakdown Insurance)",
             description: "Covers repair costs for mechanical and electrical failures after your manufacturer's warranty expires.",
             price: "10000",
+            image: "mechanicalBreakdown.jpg",
         },
         {
             name: "Comprehensive Car Insurance",
             description: "Provides the highest level of protection, covering damage to your car, other vehicles, and property, as well as theft and natural disasters.",
             price: "7,000",
+            image: "comprehensive.jpg",
         },
         {
             name: "Third Party Car Insurance",
             description: "Basic coverage that protects you against costs from damage caused to other people's vehicles or property in an accident.",
             price: "4,000",
+            image: "/thirdParty.jpg",
         }
 
     ]
@@ -28,8 +31,16 @@ export default function card() {
                     return(
                         <div className={style.card} key={index}>
                             <h1>{card.name}</h1>
-                            <p>{card.description}</p>
-                            <p>Price: ${card.price}</p>
+
+                            <div className={style.describe}>
+                                <p className={style.description}>{card.description}</p>
+
+                                <div className={style.display}>
+                                    <img className={style.cardImage} src={card.image} alt={card.name} />
+                                    <p>Price: ${card.price}</p>
+                                </div>
+                            </div>
+                            
                         </div>
                     )
                 })
