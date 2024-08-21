bcd918da90dd490bd17a19544faa7decedf74e7e
Matias Pereyra <matiasv@missionreadyhq.com>
2024-08-21 23:43:58 +1200

    fix chat bugs

--- backend/Controller/Chat.js
+++ backend/Controller/Chat.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY );
 
+
 module.exports.conversation = async(req, res) => {
   
-  const { message } = req.body;
-  console.log(message)
+  
+  
 
   try {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -26,21 +27,28 @@ module.exports.conversation = async(req, res) => {
           role: "model",
           parts: [
             {
-             text: `I’m Tinnie.  I help you to choose an insurance policy.  
-                    May I ask you a few personal questions to make sure I 
-                    recommend the best policy for you?`
+              text: `In the beginning of the conversation, introduce youself as Tinnie, and ask for permission to the user if agree to answer a few personal questions base on deciding which insurance police should be chosen out of mechanical breakdown insurance, comprehensive car insurance and third party car insurance. If user disagree, ask the user if there is anything else you could help with. There are 2 business rules: MBI is not available to trucks and racing cars.  And Comprehensive Car Insurance is only available to any motor vehicles less than 10 years old. At the end, you should recommend one or more insurance products to the user and provide reasons to support the recommendation. `
             }
           ],
         }
       ]
     })
 
-    const result = await chat.sendMessage(message);
-    
-    const response = await result.response;
-    const text = response.text();
+    async function askAndResponse(){
 
-    res.status(200).json({ message: text });
+      const { message } = req.body;
+      console.log(message)
+        const result = await chat.sendMessage(message);
+        console.log(result)
+        
+        const response = await result.response;
+        const text =  response.text();
+        res.status(200).json({ message: text });
+        //askAndResponse();
+    }
+  
+    askAndResponse();
+
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred while processing your request.' });
