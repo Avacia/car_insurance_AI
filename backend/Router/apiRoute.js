f81866ddb1e65deb22c545fb1b58ec55914ce407
Avacia <yenw@missionreadyhq.com>
2024-08-21 20:19:44 +1200

    Fixing error for AI chatbot

--- backend/Router/apiRoute.js
+++ backend/Router/apiRoute.js
@@ -2,7 +2,6 @@ const express= require('express');
 const router = express.Router();
 
 const chatController = require('../Controller/Chat');
-router.get('/chat', chatController.handler);
 router.post('/conversation', chatController.conversation);
 
 module.exports = router;
